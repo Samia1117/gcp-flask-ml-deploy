7e0bae4b328f8fa222047c34b6e9d03819894382
Samzy <48843553+Samia1117@users.noreply.github.com>
2021-07-14 10:13:09 -0500

    fix space/tab issue

--- main.py
+++ main.py
@@ -6,20 +6,20 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-	"""Return a friendly HTTP greeting"""
-	print("I am inside hello!")
-	return "Hello World (of Continuous Delivery); Edit 2"
+    """Return a friendly HTTP greeting"""
+    print("I am inside hello!")
+    return "Hello World (of Continuous Delivery); Edit 2"
 
 @app.route('/echo/<name>')
 def echo(name):
-	print(f"This was placed in the url: new-{name}")
-	val = {"new-name":name}
-	return jsonify(val)
+    print(f"This was placed in the url: new-{name}")
+    val = {"new-name":name}
+    return jsonify(val)
 
 @app.route('/pandas')
 def pandas_table():
-	df = pd.read_csv("https://raw.githubusercontent.com/noahgift/sugar/master/data/education_sugar_cdc_2003.csv")
-	return jsonify(df.to_dict())
+    df = pd.read_csv("https://raw.githubusercontent.com/noahgift/sugar/master/data/education_sugar_cdc_2003.csv")
+    return jsonify(df.to_dict())
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=8080, debug=True)
